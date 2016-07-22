2e557188306847bff5bf26c314b69ead285fece3
felix <felixbouynot@gmail.com>
2016-07-22 18:16:15 +0200

    Upload FTP finalisé

--- UploadFtp.py
+++ UploadFtp.py
@@ -1,56 +1,21 @@
-# -------------------------------------------------------------#
-#        Exemple de script d'acès en FTP à un server           #
-#                      vesrion béta                            #
-# -------------------------------------------------------------#
-
 from ftplib import FTP
+import time
 
 # les paramètres de connexion au serveur FTP
 
 ftp_host = '192.168.1.43'
-ftp_login = 'fbouynot'   # configuré sur le serveur avec son pwd
+ftp_login = 'admin'
 ftp_password = 'Azerty1234'
 
 
-path = '.'
+path = '/audits'
 ftp = FTP(ftp_host, ftp_login, ftp_password)
-ftp.debug(7)
-
-
 print(ftp.getwelcome())
-print("Contenu du répertoire courant :")
-print(ftp.dir())
-print("Changement de répertoire ")
 ftp.cwd(path)
 
-print(ftp.mkd("test"))
-print("Contenu après changement de répertoire :")
-print(ftp.dir())
-
-print("Renommer le fichier test.txt du répertoire en cours :")
-ftp.rename('test.txt', 'test_bis.txt')
-print("Après avoir renommé le fichier :")
-print(ftp.nlst())
-
-print("Renommer le fichier test.txt du répertoire en cours :")
-ftp.rename('test_bis.txt', 'test.txt')
-print("Après avoir renommé le fichier :")
-print(ftp.nlst())
-
-print("Effacement d'un fichier")
-ftp.delete('test.txt')
-print("Après effacement :")
-print(ftp.nlst())
-
-print("Création d'un répertoire :")
-ftp.mkd('MYRep')
-print("Après création :")
-print(ftp.nlst())
-
-print("Effacement du répertoire fichier :")
-ftp.rmd('MYRep')
-print("Après effacement du répertoire:")
-print(ftp.dir())
+f = open('scan.txt', 'rb')
+ftp.storbinary("STOR pentest.%s.txt" % time.time(), f)
+f.close()
 
 ftp.quit()
 ftp.close()
